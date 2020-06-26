62cabf8bf8f2b831ef42db6038938aa803d8b4c7
sammirika <1755994546@qq.com>
2020-06-27 01:00:37 +0800

    6.27打卡

---
+++ Leetcode/src/array/firstMissingPositive_41.java
@@ -0,0 +1,44 @@
+package array;
+
+
+/**
+ * 给你一个未排序的整数数组，请你找出其中没有出现的最小的正整数。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,0]
+ * 输出: 3
+ *
+ * 你的算法的时间复杂度应为O(n)，并且只能使用常数级别的额外空间。
+ *
+ * *****/
+public class firstMissingPositive_41 {
+
+    public static void main(String[] args) {
+
+    }
+
+    //利用hash的特性
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
