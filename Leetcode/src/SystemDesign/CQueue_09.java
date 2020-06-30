3ff8a2c7aff0008dea7f56a1c344f9e856ead852
sammirika <1755994546@qq.com>
2020-06-30 21:04:11 +0800

    630打卡

---
+++ Leetcode/src/SystemDesign/CQueue_09.java
@@ -0,0 +1,42 @@
+package SystemDesign;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ *
+ * ****/
+public class CQueue_09 {
+
+    LinkedList<Integer> stack1;
+    LinkedList<Integer> stack2;
+
+    public CQueue_09() {
+        stack1 = new LinkedList<>();
+        stack2 = new LinkedList<>();
+    }
+
+    public void appendTail(int value) {
+        stack1.add(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()) {
+            if (stack1.isEmpty()) return -1;
+            while (!stack1.isEmpty()) {
+                stack2.add(stack1.pop());
+            }
+            return stack2.pop();
+        } else return stack2.pop();
+    }
+}
