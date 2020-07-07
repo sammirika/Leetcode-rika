d1835da688a3c7b4c6091e588bd983b9fca94b66
sammirika <1755994546@qq.com>
2020-07-07 22:38:11 +0800

    修改包名

--- Leetcode/src/HuisuDigui/combinationSum_39.java
+++ Leetcode/src/HuisuDigui/combinationSum_39.java
@@ -1,8 +1,7 @@
-package Huisu;
+package HuisuDigui;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 /**
--- Leetcode/src/HuisuDigui/permute_46.java
+++ Leetcode/src/HuisuDigui/permute_46.java
@@ -1,4 +1,4 @@
-package Huisu;
+package HuisuDigui;
 
 import java.util.ArrayList;
 import java.util.List;
