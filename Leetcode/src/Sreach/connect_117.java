30e4b33ff5cca1dc79049c7f8112241e70d53512
sammirika <1755994546@qq.com>
2020-09-28 19:47:17 +0800

    9.28打卡，二叉树层序遍历类似问题

---
+++ Leetcode/src/Sreach/connect_117.java
@@ -0,0 +1,76 @@
+package Sreach;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/***
+ * 给定一个二叉树
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * 进阶：
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * 示例：
+ * 输入：root = [1,2,3,4,5,null,7]
+ * 输出：[1,#,2,3,#,4,5,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ * *****/
+public class connect_117 {
+
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        // 利用层序来构建
+        Queue<Node> queue = new LinkedList<>();
+        if (root == null){
+            return null;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            // 保存每一层的节点
+            Node pre = null;
+            for (int i=0;i<size;i++){
+                Node node = queue.poll();
+                if (pre != null){
+                    pre.next = node;
+                }
+                pre = node;
+                if (node.left!=null){
+                    queue.add(node.left);
+                }
+                if (node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}
