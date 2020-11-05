0fbfacc48f53fd660b8ca204796583fb7c5e96d0
sammirika <1755994546@qq.com>
2020-11-05 21:55:56 +0800

    1105打卡，字符串层序广搜

---
+++ Leetcode/src/String/ladderLength_127.java
@@ -0,0 +1,74 @@
+package String;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给定两个单词（beginWord 和 endWord）和一个字典，找到从 beginWord 到 endWord 的最短转换序列的长度。转换需遵循如下规则：
+ * 每次转换只能改变一个字母。
+ * 转换过程中的中间单词必须是字典中的单词。
+ * 说明:
+ *
+ * 如果不存在这样的转换序列，返回 0。
+ * 所有单词具有相同的长度。
+ * 所有单词只由小写字母组成。
+ * 字典中不存在重复的单词。
+ * 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+ * 示例 1:
+ *
+ * 输入:
+ * beginWord = "hit",
+ * endWord = "cog",
+ * wordList = ["hot","dot","dog","lot","log","cog"]
+ *
+ * 输出: 5
+ *
+ * 解释: 一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+ *      返回它的长度 5。
+ *
+ * ***/
+public class ladderLength_127 {
+
+    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (wordList.size() < 1) {
+            return 0;
+        }
+        int res = 0;
+        boolean[] visit = new boolean[wordList.size() + 1];
+        // 广度优先搜索
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            res++;
+            for (int i = 0; i < size; i++) {
+                String A = queue.poll();
+                if (A.equals(endWord)) {
+                    return res;
+                }
+                for (int j = 0; j < wordList.size(); j++) {
+                    if (visit[j]) {
+                        continue;
+                    }
+                    if (canChange(A, wordList.get(j))) {
+                        visit[j] = true;
+                        queue.add(wordList.get(j));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    // 判断是否能一步转化
+    private static boolean canChange(String A, String B) {
+        int diff = 0;
+        for (int i = 0; i < A.length(); i++) {
+            if (A.charAt(i) != B.charAt(i)) {
+                diff++;
+            }
+        }
+        return diff == 1;
+    }
+}
