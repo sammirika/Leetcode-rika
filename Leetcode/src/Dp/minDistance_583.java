f4f90e64a0e29ec2c83b2be7a1f3acec7e91946b
sammirika <1755994546@qq.com>
2021-03-08 23:22:01 +0800

    3.08打卡，dp问题复习

---
+++ Leetcode/src/Dp/minDistance_583.java
@@ -0,0 +1,38 @@
+package Dp;
+
+
+/****
+ *
+ * 给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，
+ * 每步可以删除任意一个字符串中的一个字符。
+ *
+ * 示例：
+ *
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ */
+
+public class minDistance_583 {
+
+    public static int minDistance(String word1, String word2) {
+        // 使用Dp来解决
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return m + n - dp[m][n] * 2;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minDistance("sea", "eat"));
+    }
+}
