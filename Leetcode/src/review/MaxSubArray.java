39e3cb82e3d16e1b57375ec929f541fb2a613f88
sammirika <1755994546@qq.com>
2021-03-12 22:59:06 +0800

    3.12,DFS复习

--- Leetcode/src/review/MaxSubArray.java
+++ Leetcode/src/review/MaxSubArray.java
@@ -1,5 +1,6 @@
 package review;
 
+import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -17,13 +18,13 @@ public class MaxSubArray {
     public static int maxSubArray(int[] nums) {
         int max = nums[0];
         int sum = 0;
-        for(int num:nums){
-            if(sum>0){
+        for (int num : nums) {
+            if (sum > 0) {
                 sum += num;
-            }else{
+            } else {
                 sum = num;
             }
-            max = Math.max(max,sum);
+            max = Math.max(max, sum);
         }
         return max;
     }
@@ -40,15 +41,66 @@ public class MaxSubArray {
      * **/
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        for (int i=0;i<m;i++){
+        for (int i = 0; i < m; i++) {
             priorityQueue.add(nums1[i]);
         }
-        for (int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             priorityQueue.add(nums2[i]);
         }
-        for (int i=0;i<m+n;i++){
+        for (int i = 0; i < m + n; i++) {
             nums1[i] = priorityQueue.poll();
         }
+    }
+
+
+    /**
+     * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+     * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+     * 此外，你可以假设该网格的四条边均被水包围。
+     * <p>
+     * 输入：grid = [
+     * ["1","1","1","1","0"],
+     * ["1","1","0","1","0"],
+     * ["1","1","0","0","0"],
+     * ["0","0","0","0","0"]
+     * ]
+     * 输出：1
+     * <p>
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/2020-top-interview-questions/xo4mt6/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     **/
+    private boolean[][] visit;
+    private int res;
+    private int m;
+    private int n;
+
+    public int numIslands(char[][] grid) {
+        res = 0;
+        visit = new boolean[grid.length][grid[0].length];
+        m = grid.length;
+        n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visit[i][j] && grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= m || j >= n || visit[i][j] || grid[i][j] == '0') {
+            return;
+        }
+        visit[i][j] = true;
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
     }
 
     public static void main(String[] args) {
