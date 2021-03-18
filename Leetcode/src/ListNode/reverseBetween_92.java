d53dc2f633c57592603de15e1acba075b11a9479
sammirika <1755994546@qq.com>
2021-03-18 23:24:52 +0800

    3.18打卡,反转链表

---
+++ Leetcode/src/ListNode/reverseBetween_92.java
@@ -0,0 +1,54 @@
+package ListNode;
+
+import day.ListNode;
+
+/**
+ * 给你单链表的头指针 head 和两个整数 left 和 right ，其中 left <= right 。
+ * 请你反转从位置 left 到位置 right 的链表节点，返回 反转后的链表 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5], left = 2, right = 4
+ * 输出：[1,4,3,2,5]
+ **/
+public class reverseBetween_92 {
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode pre = dummyNode;
+        for (int i = 0; i < left - 1; i++) {
+            pre = pre.next;
+        }
+        ListNode rightNode = pre;
+        for (int i = 0; i < right - left + 1; i++) {
+            rightNode = rightNode.next;
+        }
+        ListNode leftNode = pre.next;
+        ListNode curr = rightNode.next;
+
+        // 截断
+        pre.next = null;
+        rightNode.next = null;
+
+        reverseList(leftNode);
+
+        pre.next = rightNode;
+        leftNode.next = curr;
+        return dummyNode.next;
+    }
+
+
+    // 反转一个链表
+    private void reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+    }
+}
