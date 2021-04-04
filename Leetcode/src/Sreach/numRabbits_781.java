d61178baf6b301dc3e94824660cdd2ccd6a7eaff
sammirika <1755994546@qq.com>
2021-04-04 11:38:38 +0800

    4.03打卡，脑筋急转弯

---
+++ Leetcode/src/Sreach/numRabbits_781.java
@@ -0,0 +1,42 @@
+package Sreach;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 森林中，每个兔子都有颜色。其中一些兔子（可能是全部）告诉你还有多少其他的兔子和自己有相同的颜色。
+ * 我们将这些回答放在 answers 数组里。
+ * 返回森林中兔子的最少数量。
+ * <p>
+ * 示例:
+ * 输入: answers = [1, 1, 2]
+ * 输出: 5
+ * 解释:
+ * 两只回答了 "1" 的兔子可能有相同的颜色，设为红色。
+ * 之后回答了 "2" 的兔子不会是红色，否则他们的回答会相互矛盾。
+ * 设回答了 "2" 的兔子为蓝色。
+ * 此外，森林中还应有另外 2 只蓝色兔子的回答没有包含在数组中。
+ * 因此森林中兔子的最少数量是 5: 3 只回答的和 2 只没有回答的。
+ * <p>
+ * 输入: answers = [10, 10, 10]
+ * 输出: 11
+ * <p>
+ * 输入: answers = []
+ * 输出: 0
+ */
+public class numRabbits_781 {
+
+    public int numRabbits(int[] answers) {
+        int res = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : answers) {
+            if (map.containsKey(num) && map.get(num) > 0) {
+                map.put(num, map.get(num) - 1);
+            } else {
+                res += num + 1;
+                map.put(num, num);
+            }
+        }
+        return res;
+    }
+}
