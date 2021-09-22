dd30d00038b760437d2a9a7da41969445cfa3f66
sammirika <1755994546@qq.com>
2021-09-22 13:55:58 +0800

    2021.09.22打卡，链表分割问题

---
+++ Leetcode/src/ListNode/splitListToParts_725.java
@@ -0,0 +1,60 @@
+package ListNode;
+
+/**
+ * 给你一个头结点为 head 的单链表和一个整数 k ，请你设计一个算法将链表分隔为 k 个连续的部分。
+ * 每部分的长度应该尽可能的相等：任意两部分的长度差距不能超过 1 。这可能会导致有些部分为 null 。
+ * 这 k 个部分应该按照在链表中出现的顺序排列，并且排在前面的部分的长度应该大于或等于排在后面的长度。
+ * 返回一个由上述 k 部分组成的数组。
+ *  
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3], k = 5
+ * 输出：[[1],[2],[3],[],[]]
+ * 解释：
+ * 第一个元素 output[0] 为 output[0].val = 1 ，output[0].next = null 。
+ * 最后一个元素 output[4] 为 null ，但它作为 ListNode 的字符串表示是 [] 。
+ */
+public class splitListToParts_725 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] array = new ListNode[k];
+        int len = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            len++;
+            cur = cur.next;
+        }
+        int mod = len % k;
+        int size = len / k;
+        cur = head;
+        for (int i = 0; i < k && cur != null; i++) {
+            array[i] = cur;
+            int curSize = size + (mod-- > 0 ? 1 : 0);
+            for (int j = 0; j < curSize - 1; j++) {
+                cur = cur.next;
+            }
+            ListNode next = cur.next;
+            cur.next = null;
+            cur = next;
+        }
+        return array;
+    }
+}
