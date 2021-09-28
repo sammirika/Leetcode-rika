3a08a9bc208128454c91ddb3f151d485a298540f
sammirika <1755994546@qq.com>
2021-09-28 09:21:01 +0800

    2021.0928打卡，二叉树递归

--- Leetcode/src/HuisuDigui/pathSum_437.java
+++ Leetcode/src/HuisuDigui/pathSum_437.java
@@ -1,56 +1,57 @@
 package HuisuDigui;
 
 import day.TreeNode;
+
 /**
  * 给定一个二叉树，它的每个结点都存放着一个整数值。
- *
+ * <p>
  * 找出路径和等于给定数值的路径总数。
- *
+ * <p>
  * 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
- *
+ * <p>
  * 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
- *
+ * <p>
  * 示例：
- *
+ * <p>
  * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
- *
- *       10
- *      /  \
- *     5   -3
- *    / \    \
- *   3   2   11
- *  / \   \
+ * <p>
+ * 10
+ * /  \
+ * 5   -3
+ * / \    \
+ * 3   2   11
+ * / \   \
  * 3  -2   1
- *
+ * <p>
  * 返回 3。和等于 8 的路径有:
- *
+ * <p>
  * 1.  5 -> 3
  * 2.  5 -> 2 -> 1
  * 3.  -3 -> 11
- *
- * *****/
+ *****/
 public class pathSum_437 {
 
     int res = 0;
+
     public int pathSum(TreeNode root, int sum) {
-        if (root == null){
+        if (root == null) {
             return 0;
         }
-        getPath(root,sum);
-        pathSum(root.left,sum);
-        pathSum(root.right,sum);
+        getPath(root, sum);
+        pathSum(root.left, sum);
+        pathSum(root.right, sum);
         return res;
     }
 
-    private void getPath(TreeNode node, int num){
-        if (node==null){
+    private void getPath(TreeNode node, int num) {
+        if (node == null) {
             return;
         }
         num -= node.val;
-        if (num == 0){
-            res++;
+        if (num == 0) {
+            res += 1;
         }
-        getPath(node.left,num);
-        getPath(node.right,num);
+        getPath(node.left, num);
+        getPath(node.right, num);
     }
 }
