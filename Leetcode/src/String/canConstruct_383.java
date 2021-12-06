aaab1e0bafd6064241911ba87432af71dd75e7ed
sammirika <1755994546@qq.com>
2021-12-06 10:02:10 +0800

    2021.12.06 补充

---
+++ Leetcode/src/String/canConstruct_383.java
@@ -0,0 +1,36 @@
+package String;
+
+/**
+ * 为了不在赎金信中暴露字迹，从杂志上搜索各个需要的字母，组成单词来表达意思。
+ * 给你一个赎金信 (ransomNote) 字符串和一个杂志(magazine)字符串，
+ * 判断 ransomNote 能不能由 magazines 里面的字符构成。
+ * 如果可以构成，返回 true ；否则返回 false 。
+ * magazine 中的每个字符只能在 ransomNote 中使用一次。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：ransomNote = "a", magazine = "b"
+ * 输出：false
+ */
+public class canConstruct_383 {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] hash = new int[26];
+        for (char ch : ransomNote.toCharArray()) {
+            hash[ch - 'a']++;
+        }
+        for (char ch : magazine.toCharArray()) {
+            if (hash[ch - 'a'] > 0) {
+                hash[ch - 'a']--;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
