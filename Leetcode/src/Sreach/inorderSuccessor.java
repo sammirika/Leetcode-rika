e5228580cd8469c719abbf1e951278f6679fbcb7
sammirika <1755994546@qq.com>
2022-05-16 09:17:21 +0800

    2021.05.16打卡,二叉搜索树递归

---
+++ Leetcode/src/Sreach/inorderSuccessor.java
@@ -0,0 +1,41 @@
+package Sreach;
+
+/**
+ * 设计一个算法，找出二叉搜索树中指定节点的“下一个”节点（也即中序后继）。
+ * 如果指定节点没有对应的“下一个”节点，则返回null。
+ * 示例 1:
+ * <p>
+ * 输入: root = [2,1,3], p = 1
+ * <p>
+ * 2
+ * / \
+ * 1   3
+ * <p>
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,null,1], p = 6
+ * <p>
+ * 5
+ * / \
+ * 3   6
+ * / \
+ * 2   4
+ * /
+ * 1
+ * <p>
+ * 输出: null
+ */
+public class inorderSuccessor {
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val <= p.val) {
+            return inorderSuccessor(root.right, p);
+        }
+        TreeNode node = inorderSuccessor(root.left, p);
+        return node == null ? root : node;
+    }
+}
