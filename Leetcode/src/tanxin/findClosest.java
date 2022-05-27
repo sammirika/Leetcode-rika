a96989d11c586751aa63dfdc7eff14a94235b50c
sammirika <1755994546@qq.com>
2022-05-27 10:06:31 +0800

    2021.05.27打卡,单词最短距离

---
+++ Leetcode/src/tanxin/findClosest.java
@@ -0,0 +1,32 @@
+package tanxin;
+
+/**
+ * 有个内含单词的超大文本文件，给定任意两个不同的单词，找出在这个文件中这两个单词的最短距离(相隔单词数)。
+ * 如果寻找过程在这个文件中会重复多次，而每次寻找的单词不同，你能对此优化吗?
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：words = ["I","am","a","student","from","a","university","in","a","city"], word1 = "a", word2 = "student"
+ * 输出：1
+ * 提示：
+ * <p>
+ * words.length <= 100000
+ */
+public class findClosest {
+
+    public int findClosest(String[] words, String word1, String word2) {
+        int start = -10000;
+        int end = 10000;
+        int min = 10000;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1)) {
+                start = i;
+            }
+            if (words[i].equals(word2)) {
+                end = i;
+            }
+            min = Math.min(min, Math.abs(end-start));
+        }
+        return min;
+    }
+}
