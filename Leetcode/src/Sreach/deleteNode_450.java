2c22126289baf1b74006c495ac68d7e5ff017bf0
sammirika <1755994546@qq.com>
2022-06-02 10:08:36 +0800

    2021.06.02打卡,二叉搜索树删除指定节点

---
+++ Leetcode/src/Sreach/deleteNode_450.java
@@ -0,0 +1,71 @@
+package Sreach;
+
+/**
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。
+ * 返回二叉搜索树（有可能被更新）的根节点的引用。
+ * 一般来说，删除节点可分为两个步骤：
+ * 首先找到需要删除的节点；
+ * 如果找到了，删除它。
+ *  
+ * <p>
+ * 示例 1:
+ * 输入：root = [5,3,6,2,4,null,7], key = 3
+ * 输出：[5,4,6,2,null,null,7]
+ * 解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+ * 一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+ * 另一个正确答案是 [5,2,6,null,4,null,7]。
+ * <p>
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,7], key = 0
+ * 输出: [5,3,6,2,4,null,7]
+ * 解释: 二叉树不包含值为 0 的节点
+ * 示例 3:
+ * <p>
+ * 输入: root = [], key = 0
+ * 输出: []
+ */
+public class deleteNode_450 {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            if (root.left == null && root.right == null) {
+                return null;
+            } else if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                TreeNode successor = minimum(root.right);
+                successor.right = removeMin(root.right);
+                successor.left = root.left;
+                root.left = root.right = null;
+                return successor;
+            }
+        }
+        root.left = deleteNode(root.left, key);
+        root.right = deleteNode(root.right, key);
+        return root;
+    }
+
+    private TreeNode removeMin(TreeNode node) {
+        if (node.left == null) {
+            TreeNode rightNode = node.right;
+            node.right = null;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    private TreeNode minimum(TreeNode node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+}
