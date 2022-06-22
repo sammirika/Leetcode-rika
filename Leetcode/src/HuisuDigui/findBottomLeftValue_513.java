7d84a85d14e340a8420db9e490f761dbc8512c8f
sammirika <1755994546@qq.com>
2022-06-22 10:04:33 +0800

    2021.06.22打卡,二叉树搜索

---
+++ Leetcode/src/HuisuDigui/findBottomLeftValue_513.java
@@ -0,0 +1,50 @@
+package HuisuDigui;
+
+import day.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 给定一个二叉树的 根节点 root，请找出该二叉树的 最底层 最左边 节点的值。
+ * 假设二叉树中至少有一个节点。
+ * 示例 1:
+ * <p>
+ * <p>
+ * <p>
+ * 输入: root = [2,1,3]
+ * 输出: 1
+ * 示例 2:
+ * <p>
+ * <p>
+ * <p>
+ * 输入: [1,2,3,4,null,5,6,null,null,7]
+ * 输出: 7
+ */
+public class findBottomLeftValue_513 {
+
+    public static int findBottomLeftValue(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            return root.val;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            root = queue.poll();
+            if (root.right != null) {
+                queue.offer(root.right);
+            }
+            if (root.left != null) {
+                queue.offer(root.left);
+            }
+        }
+        return root.val;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(2);
+        node.left = new TreeNode(1);
+        node.right = new TreeNode(3);
+        System.out.println(findBottomLeftValue(node));
+    }
+}
