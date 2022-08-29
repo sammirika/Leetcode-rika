6f51ec2d1553af4d8c62711e935b338f68766232
sammirika <1755994546@qq.com>
2022-08-29 09:53:33 +0800

    2021.08.29打卡,简单重排序

---
+++ Leetcode/src/Sort/shuffle_1407.java
@@ -0,0 +1,44 @@
+package Sort;
+
+import java.util.Stack;
+
+/**
+ * 给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+ * 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,5,1,3,4,7], n = 3
+ * 输出：[2,3,5,4,1,7]
+ * 解释：由于 x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 ，所以答案为 [2,3,5,4,1,7]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3,4,4,3,2,1], n = 4
+ * 输出：[1,4,2,3,3,2,4,1]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1,1,2,2], n = 2
+ * 输出：[1,2,1,2]
+ */
+public class shuffle_1407 {
+
+    public int[] shuffle(int[] nums, int n) {
+        // 重新排序
+        int[] ret = new int[2 * n];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            ret[index++] = nums[i];
+            ret[index++] = nums[n + i];
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        shuffle_1407 kobe = new shuffle_1407();
+        int[] ret = kobe.shuffle(new int[]{2, 5, 1, 3, 4, 7}, 3);
+        for (int i : ret) {
+            System.out.print(" ");
+            System.out.print(i);
+        }
+    }
+}
