6397a965d4e97722f7f0cf37cba62ac4d8ebb3d2
sammirika <1755994546@qq.com>
2022-09-29 11:25:50 +0800

    2021.09.29打卡,字符串问题

---
+++ Leetcode/src/String/isFlipedString.java
@@ -0,0 +1,31 @@
+package String;
+
+/**
+ * 字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+ * 示例1:
+ * <p>
+ * 输入：s1 = "waterbottle", s2 = "erbottlewat"
+ * 输出：True
+ * 示例2:
+ * <p>
+ * 输入：s1 = "aa", s2 = "aba"
+ * 输出：False
+ * 提示：
+ * <p>
+ * 字符串长度在[0, 100000]范围内。
+ * 说明:
+ * <p>
+ * 你能只调用一次检查子串的方法吗？
+ */
+public class isFlipedString {
+
+    public boolean isFlipedString(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if (len1 != len2) {
+            return false;
+        }
+        String str = s2 + s2;
+        return str.contains(s1);
+    }
+}
