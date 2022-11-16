75730b71c0727f8ff3cb7be4c10269bc685b4cb0
sammirika <1755994546@qq.com>
2022-11-16 10:16:24 +0800

    2021.11.16打卡,脑筋急转弯，数组倒置

---
+++ Leetcode/src/Brainteasers/isIdealPermutation_775.java
@@ -0,0 +1,35 @@
+package Brainteasers;
+
+/**
+ * 给你一个长度为 n 的整数数组 nums ，表示由范围 [0, n - 1] 内所有整数组成的一个排列。
+ * 全局倒置 的数目等于满足下述条件不同下标对 (i, j) 的数目：
+ * 0 <= i < j < n
+ * nums[i] > nums[j]
+ * 局部倒置 的数目等于满足下述条件的下标 i 的数目：
+ *
+ * 0 <= i < n - 1
+ * nums[i] > nums[i + 1]
+ * 当数组 nums 中 全局倒置 的数量等于 局部倒置 的数量时，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,0,2]
+ * 输出：true
+ * 解释：有 1 个全局倒置，和 1 个局部倒置。
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,0]
+ * 输出：false
+ * 解释：有 2 个全局倒置，和 1 个局部倒置。
+ */
+public class isIdealPermutation_775 {
+
+    public boolean isIdealPermutation(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=i-2||nums[i]>=i+2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
