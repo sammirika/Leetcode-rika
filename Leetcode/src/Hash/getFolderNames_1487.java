40ca61e3e3166d68e9711f7d589f32f7fe03ffdd
sammirika <1755994546@qq.com>
2023-03-03 10:20:11 +0800

    2023.03.03, 字符串hash问题

---
+++ Leetcode/src/Hash/getFolderNames_1487.java
@@ -0,0 +1,69 @@
+package Hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给你一个长度为 n 的字符串数组 names 。你将会在文件系统中创建 n 个文件夹：在第 i 分钟，新建名为 names[i] 的文件夹。
+ * 由于两个文件 不能 共享相同的文件名，因此如果新建文件夹使用的文件名已经被占用，
+ * 系统会以 (k) 的形式为新文件夹的文件名添加后缀，其中 k 是能保证文件名唯一的 最小正整数 。
+ * 返回长度为 n 的字符串数组，其中 ans[i] 是创建第 i 个文件夹时系统分配给该文件夹的实际名称。
+ * 示例 1：
+ * <p>
+ * 输入：names = ["pes","fifa","gta","pes(2019)"]
+ * 输出：["pes","fifa","gta","pes(2019)"]
+ * 解释：文件系统将会这样创建文件名：
+ * "pes" --> 之前未分配，仍为 "pes"
+ * "fifa" --> 之前未分配，仍为 "fifa"
+ * "gta" --> 之前未分配，仍为 "gta"
+ * "pes(2019)" --> 之前未分配，仍为 "pes(2019)"
+ * 示例 2：
+ * <p>
+ * 输入：names = ["gta","gta(1)","gta","avalon"]
+ * 输出：["gta","gta(1)","gta(2)","avalon"]
+ * 解释：文件系统将会这样创建文件名：
+ * "gta" --> 之前未分配，仍为 "gta"
+ * "gta(1)" --> 之前未分配，仍为 "gta(1)"
+ * "gta" --> 文件名被占用，系统为该名称添加后缀 (k)，由于 "gta(1)" 也被占用，所以 k = 2 。实际创建的文件名为 "gta(2)" 。
+ * "avalon" --> 之前未分配，仍为 "avalon"
+ * 示例 3：
+ * <p>
+ * 输入：names = ["onepiece","onepiece(1)","onepiece(2)","onepiece(3)","onepiece"]
+ * 输出：["onepiece","onepiece(1)","onepiece(2)","onepiece(3)","onepiece(4)"]
+ * 解释：当创建最后一个文件夹时，最小的正有效 k 为 4 ，文件名变为 "onepiece(4)"。
+ * 示例 4：
+ * <p>
+ * 输入：names = ["wano","wano","wano","wano"]
+ * 输出：["wano","wano(1)","wano(2)","wano(3)"]
+ * 解释：每次创建文件夹 "wano" 时，只需增加后缀中 k 的值即可。
+ */
+public class getFolderNames_1487 {
+
+    public String[] getFolderNames(String[] names) {
+        Map<String, Integer> map = new HashMap<>();
+        String[] ret = new String[names.length];
+        int index = 0;
+        for (String name : names) {
+            Integer count = map.get(name);
+            if (count != null) {
+                int k = count + 1;
+                while (true) {
+                    String newName = name + "(" + k + ")";
+                    if (map.get(newName) == null) {
+                        // 排除已存在后缀的文件名
+                        map.put(name, k);
+                        map.put(newName, 0);
+                        ret[index++] = newName;
+                        break;
+                    } else {
+                        k++;
+                    }
+                }
+            } else {
+                map.put(name, 0);
+                ret[index++] = name;
+            }
+        }
+        return ret;
+    }
+}
